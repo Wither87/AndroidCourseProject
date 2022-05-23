d66343d3e25fc879bc09a7049af55e0f054cb1df
Wither87 <wither_87@mail.ru>
2022-05-23 17:02:05 +0500

    Added retrofit service paths

--- data/src/main/java/com/example/data/api/RetrofitService.kt
+++ data/src/main/java/com/example/data/api/RetrofitService.kt
@@ -5,17 +5,17 @@ import retrofit2.http.Path
 import com.example.data.models.*
 
 interface RetrofitService {
-    @GET("")
+    @GET("brands")
     suspend fun getBrandsList(): List<Brand>
 
-    @GET("")
-    suspend fun getPage(): Page
+    @GET("brands/{slug}?page={page_number}}")
+    suspend fun getPage(@Path("slug") slug: String, @Path("page_number") page_number: Int): Page
 
-    @GET("")
-    suspend fun getPhonesList(): List<Phone>
+    //@GET("")
+    suspend fun getPhonesList(slug: String): List<Phone>
 
-    @GET("")
-    suspend fun getPhoneDetails(): PhoneDetails
+    @GET("{phone_slug}")
+    suspend fun getPhoneDetails(@Path("phone_slug") phone_slug: String): PhoneDetails
 
     @GET("")
     suspend fun getSpecificationsList(): List<Specifications>
