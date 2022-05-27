d52431bc9d111b6d11d7396c1e57ca4211c57165
Сергей Крутолевич <wither_87@mail.ru>
2022-05-27 15:19:06 +0500

    added mapToData methods

--- data/src/main/java/com/example/data/models/MapperExt.kt
+++ data/src/main/java/com/example/data/models/MapperExt.kt
@@ -6,6 +6,7 @@ import com.example.domain.models.PhoneDetails as DomainPhoneDetails
 import com.example.domain.models.Specifications as DomainSpecifications
 import com.example.domain.models.Specs as DomainSpecs
 
+// Mappers for Brand classes
 fun Brand.mapToDomain() : DomainBrand{
     return DomainBrand(
         Brand_id = this.Brand_id,
@@ -16,6 +17,18 @@ fun Brand.mapToDomain() : DomainBrand{
     )
 }
 
+fun DomainBrand.mapToData() : Brand{
+    return Brand(
+        Brand_id = this.Brand_id,
+        Brand_name = this.Brand_name,
+        Brand_slug = this.Brand_slug,
+        Device_count = this.Device_count,
+        Detail = this.Detail
+    )
+}
+
+
+// Mappers for Phone classes
 fun Phone.mapToDomain() : DomainPhone{
     return DomainPhone(
         Brand=this.Brand,
@@ -26,6 +39,18 @@ fun Phone.mapToDomain() : DomainPhone{
     )
 }
 
+fun DomainPhone.mapToData() : Phone{
+    return Phone(
+        Brand=this.Brand,
+        Phone_name=this.Phone_name,
+        Slug=this.Slug,
+        Image=this.Image,
+        Detail=this.Detail
+    )
+}
+
+
+// Mappers for PhoneDetails classes
 fun PhoneDetails.mapToDomain() : DomainPhoneDetails{
     val specifications = mutableListOf<DomainSpecifications>()
     for (spec in this.Specifications){
@@ -44,6 +69,26 @@ fun PhoneDetails.mapToDomain() : DomainPhoneDetails{
     )
 }
 
+fun DomainPhoneDetails.mapToData() : PhoneDetails{
+    val specifications = mutableListOf<Specifications>()
+    for (spec in this.Specifications){
+        specifications.add(spec.mapToData())
+    }
+    return PhoneDetails(
+        Brand=this.Brand,
+        Phone_name=this.Phone_name,
+        Thumbnail=this.Thumbnail,
+        Phone_images=this.Phone_images,
+        Release_date=this.Release_date,
+        Dimension=this.Dimension,
+        Os=this.Os,
+        Storage=this.Storage,
+        Specifications=specifications.toList()
+    )
+}
+
+
+// Mappers for Specifications classes
 fun Specifications.mapToDomain() : DomainSpecifications{
     val specs = mutableListOf<DomainSpecs>()
     for (spec in this.Specs){
@@ -55,8 +100,28 @@ fun Specifications.mapToDomain() : DomainSpecifications{
     )
 }
 
+fun DomainSpecifications.mapToData() : Specifications{
+    val specs = mutableListOf<Specs>()
+    for (spec in this.Specs){
+        specs.add(spec.mapToData())
+    }
+    return Specifications(
+        Title=this.Title,
+        Specs=specs.toList()
+    )
+}
+
+
+// Mappers for Specs classes
 fun Specs.mapToDomain() : DomainSpecs{
     return DomainSpecs(
+        Key=this.Key,
+        Val=this.Val
+    )
+}
+
+fun DomainSpecs.mapToData() : Specs{
+    return Specs(
         Key=this.Key,
         Val=this.Val
     )
