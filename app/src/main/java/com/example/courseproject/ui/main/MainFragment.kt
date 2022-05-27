31ccded90bc48f931292fc08140434eef7d5267e
Сергей Крутолевич <wither_87@mail.ru>
2022-05-27 15:04:13 +0500

    added MainViewModel

--- app/src/main/java/com/example/courseproject/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/courseproject/ui/main/MainFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.example.courseproject.R
 
 class MainFragment : Fragment() {
@@ -26,7 +27,37 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        displayBrands()
+
         // TODO: Use the ViewModel
+    }
+
+
+
+
+    private fun displayBrands(){
+        viewModel.brandsLiveData.observe(this, Observer{
+            val brands = it
+            // TODO отобразить список брендов
+        })
+        viewModel.loadBrands()
+    }
+
+    private fun displayPhones(brandSlug: String){
+        viewModel.phonesLiveData.observe(this, Observer{
+            val phones = it
+            // TODO отобразить список телефонов
+        })
+        viewModel.loadPhones(brandSlug)
+    }
+
+    private fun displayPhoneDetails(phoneSlug: String){
+        viewModel.phoneDetailsLiveData.observe(this, Observer{
+            val phoneDetails = it
+            // TODO отобразить информацию о телефоне
+        })
+        viewModel.loadPhoneDetails(phoneSlug)
     }
 
 }
--- app/src/main/java/com/example/courseproject/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/courseproject/ui/main/MainViewModel.kt
@@ -1,7 +1,47 @@
 package com.example.courseproject.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.models.Brand
+import com.example.domain.models.Phone
+import com.example.domain.models.PhoneDetails
+import com.example.domain.usecases.GetBrandsListUseCase
+import com.example.domain.usecases.GetPhoneDetailsUseCase
+import com.example.domain.usecases.GetPhonesUseCase
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class MainViewModel @Inject constructor(
+    private val getBrandsUseCase: GetBrandsListUseCase,
+    private val getPhonesUseCase: GetPhonesUseCase,
+    private val getPhoneDetailsUseCase: GetPhoneDetailsUseCase,
+) : ViewModel() {
+
+    val brandsLiveData = MutableLiveData<List<Brand>>()
+    val phonesLiveData = MutableLiveData<List<Phone>>()
+    val phoneDetailsLiveData = MutableLiveData<PhoneDetails>()
+
+    fun loadBrands() {
+        viewModelScope.launch {
+            val brands = getBrandsUseCase.execute()
+            brandsLiveData.postValue(brands)
+        }
+
+    }
+
+    fun loadPhones(brandsSlug: String) {
+        viewModelScope.launch {
+            val phones = getPhonesUseCase.execute(brandsSlug)
+            phonesLiveData.postValue(phones)
+        }
+    }
+
+    fun loadPhoneDetails(phoneSlug: String){
+        viewModelScope.launch {
+            val phoneDetails = getPhoneDetailsUseCase.execute(phoneSlug)
+            phoneDetailsLiveData.postValue(phoneDetails)
+        }
+    }
 }
